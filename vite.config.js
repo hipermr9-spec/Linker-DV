c2c675b53bfce33eb2f7742011b798b0928656bc
hipermr9-spec <hipermr9@gmail.com>
2026-06-30 19:23:46 +0300

    allow rail#### domain

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'linker-dv-production.up.railway.app'
+    ]
+  },
+  preview: {
+    allowedHosts: [
+      'linker-dv-production.up.railway.app'
+    ]
+  }
 })
