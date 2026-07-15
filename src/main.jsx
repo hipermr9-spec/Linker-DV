2a7c61a2db996994fb05d682dc5e0ebe2df656c7
hipermr9-spec <hipermr9@gmail.com>
2026-07-15 07:07:09 +0300

    v0.1.1 Production

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from '../Frontend/App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // استيراد الـ Router
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* هنا لازم تغلف التطبيق بالكامل */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
